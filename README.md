a7c95273971b031595628bd95eb871bd1f0d4b16
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2023-05-22 17:39:13 -0400

    Initial import. binki-bigtime-net-timer-quick-unpause-1.0.0.

---
+++ README.md
@@ -0,0 +1,7 @@
+Automatically performs the second click for you when pausing or unpausing a timer in bigtime.
+
+Switching between timers should just take one click. They do not by default. This script fixes that.
+
+To access the screen where you can edit a timer, simply click on the timer instead of the pause/start button.
+
+[Install](binki-bigtime-net-timer-quick-unpause.user.js?raw=1)
---
+++ binki-bigtime-net-timer-quick-unpause.user.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name     binki-bigtime-net-timer-quick-unpause
+// @version  1.0.0
+// @grant    none
+// @match https://intuit.bigtime.net/Bigtime/Entry2
+// @require https://github.com/binki/binki-userscript-when-element-changed-async/raw/master/binki-userscript-when-element-changed-async.js
+// ==/UserScript==
+
+(async () => {
+  const timerMenuList = await (async () => {
+    while (true) {
+      const timerMenuList = document.querySelector('.timerMenu > ul.list');
+      if (timerMenuList) return timerMenuList;
+      await whenElementChangedAsync(document.body);
+    }
+  })();
+  console.log('found timerMenu list', timerMenuList);
+  const timerMenuEdit = await (async () => {
+    while (true) {
+  	  const timerMenuEdit = document.querySelector('.timerMenu > ul.edit');
+      if (timerMenuEdit) return timerMenuEdit;
+      await whenElementChangedAsync(document.body);
+    }
+  })();
+  console.log('found timerMenu edit', timerMenuEdit);
+  timerMenuList.addEventListener('click', e => {
+    const button = (() => {
+      let target = e.target;
+      while (target && target.nodeName.toLowerCase() !== 'button') target = target.parentNode;
+      return target;
+    })();
+    console.log('sought under ', e.target, ' and found ', button);
+    if (!button) return;
+    // A button in the menu was clicked. So do followup action.
+    console.log('detected action click, following up with auto-click');
+    (async () => {
+      while (document.querySelector('.timerMenu > ul.edit.ng-hide')) {
+        await whenElementChangedAsync(timerMenuEdit);
+      }
+      console.log('edit screen loaded, auto-clicking…');
+      timerMenuEdit.querySelector('button.btn-primary').click();
+    })();
+  });
+})();
